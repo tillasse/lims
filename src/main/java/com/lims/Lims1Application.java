fdbf5b5627ea37189fe75787a10901a9632c2792
tillasse <mojemmi.said@gmail.com>
2018-12-16 14:16:39 +0100

    First commit

---
+++ src/main/java/com/lims/Lims1Application.java
@@ -0,0 +1,14 @@
+package com.lims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lims1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lims1Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/lims/Lims1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lims;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lims1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
